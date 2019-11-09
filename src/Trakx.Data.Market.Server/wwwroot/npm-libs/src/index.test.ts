0bf0dcd936052b2e8f9ac08eadab9d392c8921c7
monsieurleberre <monsieur.leberre@gmail.com>
2019-11-09 08:45:10 +0000

    prove tree shaking is working

--- src/Trakx.Data.Market.Server/wwwroot/npm-libs/src/index.test.ts
+++ src/Trakx.Data.Market.Server/wwwroot/npm-libs/src/index.test.ts
@@ -1,5 +1,12 @@
-﻿import { Greeter } from './index';
+﻿import { Greeter, IntersectThis } from './index';
 
 test('My Greeter', () => {
     expect(Greeter('dude')).toBe('Hello dude');
+});
+
+test('Intersector', () => {
+    const a = ['hello', 'not', 'you'];
+    const b = ['hello', 'you', 'toi'];
+    const intersection = IntersectThis(a, b);
+    expect(intersection).toBe('hello,you');
 });
--- src/Trakx.Data.Market.Server/wwwroot/npm-libs/src/index.ts
+++ src/Trakx.Data.Market.Server/wwwroot/npm-libs/src/index.ts
@@ -1,1 +1,5 @@
-﻿export const Greeter = (name: string): string => `Hello ${name}`;
+﻿import intersection from 'lodash/fp/intersection';
+
+export const Greeter = (name: string): string => `Hello ${name}`;
+
+export const IntersectThis = (a: string[], b: string[]): string => intersection(a, b).join(',');
