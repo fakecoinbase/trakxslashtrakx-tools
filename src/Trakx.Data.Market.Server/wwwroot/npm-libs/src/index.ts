7b1b8ffd6c487cedab586837c88a6af455910978
monsieurleberre <monsieur.leberre@gmail.com>
2019-11-22 08:45:39 +0000

    remove temp code

--- src/Trakx.Data.Market.Server/wwwroot/npm-libs/src/index.ts
+++ src/Trakx.Data.Market.Server/wwwroot/npm-libs/src/index.ts
@@ -1,5 +1,5 @@
-﻿import intersection from 'lodash/fp/intersection';
+﻿//import intersection from 'lodash/fp/intersection';
 
-export const Greeter = (name: string): string => `Hello ${name}`;
+//export const Greeter = (name: string): string => `Hello ${name}`;
 
-export const IntersectThis = (a: string[], b: string[]): string => intersection(a, b).join(',');
+//export const IntersectThis = (a: string[], b: string[]): string => intersection(a, b).join(',');
