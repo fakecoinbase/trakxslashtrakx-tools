cde99dbc29efee2f94620d595609682cc99c2426
monsieurleberre <monsieur.leberre@gmail.com>
2020-01-15 23:53:29 +0000

    fix build, Elapsed went from string to number in Json

--- src/Trakx.Data.Market.Common/Sources/Messari/DTOs/Status.cs
+++ src/Trakx.Data.Market.Common/Sources/Messari/DTOs/Status.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Text.Json.Serialization;
-using Trakx.Data.Market.Common.Serialisation.Converters;
 
 namespace Trakx.Data.Market.Common.Sources.Messari.DTOs
 {
     public partial class Status
     {
         [JsonPropertyName("elapsed")]
-        [JsonConverter(typeof(StringLongConverter))]
-        public long Elapsed { get; set; }
+        public long? Elapsed { get; set; }
 
         [JsonPropertyName("timestamp")]
         public DateTimeOffset Timestamp { get; set; }
