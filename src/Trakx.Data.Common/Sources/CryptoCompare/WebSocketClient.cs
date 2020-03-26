e1559eeb2d0ba1312fb7473a0973a7645e8fa02d
monsieurleberre <monsieur.leberre@gmail.com>
2020-03-26 15:51:27 +0000

    drafting

---
+++ src/Trakx.Data.Common/Sources/CryptoCompare/WebSocketClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Trakx.Data.Common.Sources.CryptoCompare
+{
+    public class SocketMessage
+    {
+        public SocketMessage(string action)
+        {
+            
+        }
+
+        [JsonPropertyName("action")]
+        public string Action { get; set; }
+
+        [JsonPropertyName("subs")]
+        public List<string> Subscriptions { get; } = new List<string>();
+
+    }
+
+    public class WebSocketClient
+    {
+        private readonly string _apiKey;
+        private ClientWebSocket _client;
+
+        public WebSocketClient(string apiKey)
+        {
+            _apiKey = apiKey;
+            _client = new ClientWebSocket();
+        }
+
+        public async Task<string> Connect()
+        {
+            string allStrings = string.Empty;
+            await _client.ConnectAsync(new Uri($"wss://streamer.cryptocompare.com/v2?api_key={_apiKey}"), CancellationToken.None);
+            var message = new SocketMessage("SubAdd");
+            message.Subscriptions.Add("5~CCCAGG~BTC~USD");
+            await _client.SendAsync(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message)),
+                WebSocketMessageType.Text, true, CancellationToken.None);
+
+            var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                var buffer = new Memory<byte>();
+                var received = await _client.ReceiveAsync(buffer, cancellationTokenSource.Token);
+                var result = Encoding.UTF8.GetString(buffer.ToArray());
+                allStrings += result;
+            }
+
+            return allStrings;
+        }
+    }
+}
---
+++ src/Trakx.Data.Tests/Integration/Common/Sources/CryptoCompare/WebSocketClientTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Trakx.Data.Common.Sources.CryptoCompare;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Trakx.Data.Tests.Integration.Common.Sources.CryptoCompare
+{
+    public class WebSocketClientTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly WebSocketClient _client;
+
+        public WebSocketClientTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _client = new WebSocketClient("");
+        }
+
+        [Fact]
+        public async Task WebSocketClient_should_receive_updates()
+        {
+            var result = await _client.Connect();
+            _output.WriteLine(result);
+        }
+    }
+}
