2d55db702ecd555a10ffc516663ed9d1948d8d37
Aymeric Noel <aym.noel@gmail.com>
2020-05-08 12:06:50 +0200

    Adding contracts for wrapped tokens (#96)
    * Adding contracts
    
    * Modif
    
    * Matthieu'suggestion, but not sure :joy:
    
    Co-authored-by: monsieurleberre <monsieurleberre@users.noreply.github.com>

---
+++ src/Trakx.Contracts/Wrapping/DeployedContractAddresses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Trakx.Contracts.Set;
+
+namespace Trakx.Contracts.Wrapping
+{
+    public static class DeployedContractAddresses
+    {
+        public static readonly string wxrp = "0xbbD8689321a543510a67e4a9F9D5e7B0C2de5257"; //rinkeby network
+
+        public static readonly ReadOnlyDictionary<string, string> AddressByName = Address.AddressByName;
+    }
+}
