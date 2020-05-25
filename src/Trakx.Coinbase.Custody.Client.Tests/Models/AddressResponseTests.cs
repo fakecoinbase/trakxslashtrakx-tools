4d62323971712690b1ce5c11828a4974250b043a
Aymeric Noel <aym.noel@gmail.com>
2020-05-25 19:27:42 +0200

    Adding models and Test solution (#113)
    * Adding models
    
    * Move to netStandard and adding nuget package system.text.json
    
    * Update src/Trakx.Coinbase.Custody.Client/Models/Transaction.cs
    
    * Update src/Trakx.Coinbase.Custody.Client/Models/Transaction.cs
    
    * Update src/Trakx.Coinbase.Custody.Client/Models/Address.cs
    
    * add a test for my sanity
    
    Co-authored-by: monsieurleberre <monsieur.leberre@gmail.com>

---
+++ src/Trakx.Coinbase.Custody.Client.Tests/Models/AddressResponseTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Trakx.Coinbase.Custody.Client.Models;
+using Xunit;
+
+namespace Trakx.Coinbase.Custody.Client.Tests.Models
+{
+    public class AddressResponseTests
+    {
+        [Fact]
+        public async Task AddressResponse_can_be_deserialised()
+        {
+            var type = typeof(AddressResponse);
+            var shortAssemblyName = type.Assembly!.FullName!.Split(",")[0]; 
+            var namespaceSuffix = type.Namespace!.Replace(shortAssemblyName+".", "");
+            var sampleResponse = await File.ReadAllTextAsync(Path.Combine(namespaceSuffix, $"{type.Name}.json"))
+                .ConfigureAwait(false);
+
+            var deserialised = JsonSerializer.Deserialize<PagedResponse<AddressResponse>>(sampleResponse);
+
+            var firstAddress = deserialised.Data[0];
+            firstAddress.Address.Should().Be("fake_btc_cold_address");
+            firstAddress.State.Should().Be(AddressState.Cold);
+            firstAddress.Balance.Should().Be(12);
+            firstAddress.BlockchainLink.Should().Be("https://live.blockcypher.com/btc/address/fake_btc_cold_address");
+            firstAddress.CreatedAt.Should().Be(new DateTimeOffset(2019, 8, 19, 18, 15, 23, 494, TimeSpan.Zero));
+            firstAddress.UpdatedAt.Should().Be(new DateTimeOffset(2019, 8, 19, 18, 15, 23, 520, TimeSpan.Zero));
+            firstAddress.Currency.Should().Be("btc");
+        }
+    }
+}
---
+++ src/Trakx.Coinbase.Custody.Client/Models/Wallet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Trakx.Coinbase.Custody.Client.Models
+{
+    public class Wallet
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("balance")]
+        public string Balance { get; set; }
+
+        [JsonPropertyName("withdrawable_balance")]
+        public string WithdrawableBalance { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [JsonPropertyName("cold_address")]
+        public string ColdAddress { get; set; }
+
+        [JsonPropertyName("currency")]
+        public string Currency { get; set; }
+    }
+}
